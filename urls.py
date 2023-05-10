df74d37f2409cb1e08b0af5a6583468409f72e7b
alexandrdvrt <94928103+alexandrdvrt@users.noreply.github.com>
2023-05-10 20:03:51 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-	path('', views.welcome, name='welcome'), 
-	path('users/', views.RegisterUserView.as_view(), name='register_user'),
+    path('', views.welcome, name='welcome'), 
+    path('users/', views.RegisterUserView.as_view(), name='register_user'),
     path('friend_requests/', views.FriendRequestCreateView.as_view(), name='friend_request_create'),
     path('friend_requests/', views.FriendRequestListView.as_view(), name='friend_request_list'),
     path('friend_requests/<int:id>/', views.FriendRequestUpdateView.as_view(), name='friend_request_update'),
